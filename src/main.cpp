0c8f935ab394cff94205c47c52d6e987610aecc3
Maciej Jakobszy <jdsk88@gmail.com>
2024-11-11 22:43:23 +0100

    custom font  implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#define AA_FONT_SMALL "coolvetica-regular"
+
+#include <FS.h>
+#include <LittleFS.h>
+
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 
@@ -5,14 +10,31 @@ TFT_eSPI tft = TFT_eSPI(); // Initialize the display
 
 void setup()
 {
+  Serial.begin(115200);
   tft.init();
-  tft.setRotation(1); // Adjust rotation (0-3) to match your display orientation
-  tft.fillScreen(TFT_BLACK);
+  tft.setRotation(3); // Adjust rotation (0-3) to match your display orientation
+
+  if (!LittleFS.begin())
+  {
+    Serial.println("An Error has occurred while mounting LittleFS :(");
+    return;
+  }
+  Serial.println("LittleFS is available!");
+  if (LittleFS.exists("/coolvetica-regular.vlw"))
+  {
+    Serial.println("Font coolvetica-regular is exist od disk!");
+  }
+
+  String fileName = "coolvetica-regular";
+  tft.loadFont(fileName, LittleFS);
+  tft.showFont(2000);
+
+  tft.fillScreen(TFT_RED);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.drawString("Hello, ESP32!", 10, 10, 4); // Display "Hello, ESP32!" at x=10, y=10
+  tft.drawString("Hello, ESP32", 10, 10, 4); // Display "Hello, ESP32!" at x=10, y=10
+  tft.drawString("Zażółć gęślą jaźń Zażółć gęślą jaźń", 10, 50, 4);
 }
 
 void loop()
 {
-  // Add animations or other display updates here
 }
